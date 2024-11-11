91124bb7c06db18bf3472044f1553bf3fe76c367
ChristianBocanegra <christianmbp95@gmail.com>
2024-11-11 12:01:41 -0500

    Functionality
    Features have been added and the graphical interface has been improved

--- ChristianBocanegra_COMP228Lab4/src/main/java/com/example/christianbocanegra_comp228lab4/HelloApplication.java
+++ ChristianBocanegra_COMP228Lab4/src/main/java/com/example/christianbocanegra_comp228lab4/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.christianbocanegra_comp228lab4;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,10 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 840, 380);
+        Image icon = new Image("https://upload.wikimedia.org/wikipedia/en/c/cc/JavaFX_Logo.png");
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- ChristianBocanegra_COMP228Lab4/src/main/java/com/example/christianbocanegra_comp228lab4/HelloController.java
+++ ChristianBocanegra_COMP228Lab4/src/main/java/com/example/christianbocanegra_comp228lab4/HelloController.java
@@ -1,14 +1,85 @@
 package com.example.christianbocanegra_comp228lab4;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField nameField, addressField, cityField, provinceField, postalCodeField, phoneField, emailField;
+
+    @FXML
+    private RadioButton csRadio, businessRadio;
+
+    @FXML
+    private ComboBox<String> courseComboBox;
+
+    @FXML
+    private ListView<String> courseListView;
+
+    @FXML
+    private CheckBox studentCouncilCheck, volunteerWorkCheck;
+
+    @FXML
+    private TextArea displayArea;
+
+    private ObservableList<String> csCourses = FXCollections.observableArrayList("Java", "Python", "C#");
+    private ObservableList<String> businessCourses = FXCollections.observableArrayList("Finance", "Marketing", "Management");
+
+    @FXML
+    public void initialize() {
+        ToggleGroup majorGroup = new ToggleGroup();
+        csRadio.setToggleGroup(majorGroup);
+        businessRadio.setToggleGroup(majorGroup);
+
+        csRadio.setOnAction(event -> courseComboBox.setItems(csCourses));
+        businessRadio.setOnAction(event -> courseComboBox.setItems(businessCourses));
+
+        courseComboBox.setOnAction(event -> addCourse());
+    }
+
+    @FXML
+    protected void addCourse() {
+        String selectedCourse = courseComboBox.getSelectionModel().getSelectedItem();
+        if (selectedCourse != null && !courseListView.getItems().contains(selectedCourse)) {
+            courseListView.getItems().add(selectedCourse);
+        }
+    }
+
+    @FXML
+    protected void displayInfo() {
+        StringBuilder studentInfo = new StringBuilder();
+        studentInfo.append(nameField.getText()).append(", ")
+                .append(addressField.getText()).append(", ")
+                .append(cityField.getText()).append(", ")
+                .append(provinceField.getText()).append(", ")
+                .append(postalCodeField.getText()).append(", ")
+                .append(phoneField.getText()).append(", ")
+                .append(emailField.getText()).append("\n");
+
+        studentInfo.append("Courses:\n");
+
+        for (String course : courseListView.getItems()) {
+            studentInfo.append(course).append("\n");
+        }
+
+        if (csRadio.isSelected()) {
+            studentInfo.append("Major: Computer Science\n");
+        } else if (businessRadio.isSelected()) {
+            studentInfo.append("Major: Business\n");
+        }
+
+        if (studentCouncilCheck.isSelected()) {
+            studentInfo.append("Involvement: Student Council\n");
+        }
+        if (volunteerWorkCheck.isSelected()) {
+            studentInfo.append("Involvement: Volunteer Work\n");
+        }
+
+        displayArea.setText(studentInfo.toString());
+
+
     }
 }
